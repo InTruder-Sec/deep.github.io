f8053201b8da9c096f678e4861f939cbfd53e1aa
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-08-08 18:13:28 +0000

    update

---
+++ src/terminal.js
@@ -0,0 +1,17 @@
+function funClose() {
+    document.getElementById("close").textContent = "x"
+}
+
+function funSmall() {
+    document.getElementById("small").textcontent = "-"
+}
+
+function funMin() {
+    document.getElementById("min").textContent = "+"
+}
+
+function remove() {
+    document.getElementById('close').textContent = "";
+    document.getElementById('small').textContent = "";
+    document.getElementById('min').textContent = "";
+}
