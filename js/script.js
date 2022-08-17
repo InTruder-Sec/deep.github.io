f6188ffc9b4bcd68d8ff198f73d2df56f0a6011d
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-08-17 18:57:14 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function displayVtf() {
+    document.getElementById("vtf-head").style.transition = "background 1s ease"
+    document.getElementById("vtf-head").style.backgroundColor = "red";
+}
+
+
+function displayOwasp() {
+    document.getElementById("owasp-head").style.transition = "background 1s ease"
+    document.getElementById("owasp-head").style.backgroundColor = "blue";
+}
+
+function displayhacker() {
+    document.getElementById("h1-head").style.transition = "background 1s ease"
+    document.getElementById("h1-head").style.backgroundColor = "purple";
+}
+
+function out() {
+    document.getElementById("vtf-head").style.transition = "background 1s ease"
+    document.getElementById("vtf-head").style.backgroundColor = "rgb(52, 45, 45)"
+    document.getElementById("owasp-head").style.transition = "background 1s ease"
+    document.getElementById("owasp-head").style.backgroundColor = "rgb(52, 45, 45)"
+    document.getElementById("h1-head").style.transition = "background 1s ease"
+    document.getElementById("h1-head").style.backgroundColor = "rgb(52, 45, 45)"
+}
+
