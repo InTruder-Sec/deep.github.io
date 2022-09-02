db30efad3d030d351e442e3e66cfcf282b6874f1
Deep Dhakate <75447837+InTruder-Sec@users.noreply.github.com>
2022-09-02 19:01:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,20 +53,30 @@ function remove() {
     document.getElementById('new_min').textContent = "";
     document.getElementById('new_small').textContent = "";
     document.getElementById('new_close').textContent = "";
+    document.getElementById('new1_min').textContent = "";
+    document.getElementById('new1_small').textContent = "";
+    document.getElementById('new1_close').textContent = "";
 }
 
 
 function vtfDiscription() {
-    document.getElementById("vtf-modal").style.display = "block";
+    document.getElementById("vtf-dis").style.height = "100%";
+    document.getElementById("exit").style.display = "block";
+    document.getElementById("close-arrow").style.transform = 'rotate(45deg)';
 }
 
 function owaspDiscription() {
     document.getElementById("owasp-modal").style.display = "block";
 }
 
+function h1Discription() {
+    document.getElementById("h1-modal").style.display = "block";
+}
+
 function closeM() {
     document.getElementById("vtf-modal").style.display = "none";
     document.getElementById("owasp-modal").style.display = "none";
+    document.getElementById("h1-modal").style.display = "none";
 }
 
 window.onclick = function(event) {
@@ -81,4 +91,30 @@ window.onclick = function(event) {
     if (event.target == owasp) {
         owasp.style.display = "none";
     }
-}     
+}  
+window.onclick = function(event) {
+    var h1 = document.getElementById("h1-modal");
+    if (event.target == h1) {
+        h1.style.display = "none";
+    }
+}   
+
+
+function new1_funClose() {
+    document.getElementById("new1_close").textContent = "x";
+}
+
+function new1_funSmall() {
+    document.getElementById("new1_small").textContent = "-";
+}
+
+function new1_funMin() {
+    document.getElementById("new1_min").textContent = "+";
+}
+
+function displayAbout() {
+    document.getElementById("about-effect").style.width = "100%";
+}
+function closevtf() {
+    document.getElementById("vtf-dis").style.height = "0%"
+}
