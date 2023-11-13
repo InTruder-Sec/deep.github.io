c09582dd411cdb419c33fe9017905bbc2ef7a0f4
= <dhakatedeep909@gmail.com>
2023-11-14 00:47:14 +0530

    Update

---
+++ portfolio/src/Data/Projects.js
@@ -0,0 +1,40 @@
+const ProjectData = [
+  {
+    id: "01",
+    key: "1",
+    title: "FullStack Web Development Projects",
+    details:
+      "I am currently working on full stack web development projects. Almost all of the websites created under this project are built with MERN Stack. Project contails multiple games, E-commerce website, normal website and much more.",
+    link: "github.com/InTruder-Sec/Full-StackDev-Projects",
+    tools: ["HTML5", "CSS", "JavaScript", "React", "Canva"],
+  },
+  {
+    id: "02",
+    key: "2",
+    title: "Iconic Hashing Algorithm",
+    details:
+      "IH is a unique hashing algorithm which works on base 10 digits for hashing. This hashing algorithm works on each of these digits to convert them into an unpredictable number format. This tool hashes any given string into its iconic hash.",
+    link: "github.com/InTruder-Sec/IHA",
+    tools: ["Python3"],
+  },
+  {
+    id: "03",
+    key: "3",
+    title: "Cesar Encrypter & Decrypter",
+    details:
+      "In cryptography, a Caesar cipher is categorized as a substitution cipher in which the alphabet in the plain text is shifted by a fixed number down the alphabet. I have created a python tool which encrypts and decrypts strings with all posible set of keys.",
+    link: "github.com/InTruder-Sec/caesar-cipher",
+    tools: ["Python3"],
+  },
+  {
+    id: "04",
+    key: "4",
+    title: "Tech-Fest 2022 Website",
+    details:
+      "Techfest - 2K22 is the technical tvent of PCET's Pimpri Chinchwad College of Engineering, Pune. Website for Tech-Fest 2k22 was created and maintained by me. Almost 2000+ event registrations were done on the site.",
+    link: "techfest2k22.web.app/",
+    tools: ["HTML5", "CSS", "JavaScript", "Firebase"],
+  },
+];
+
+export default ProjectData;
