987a2333a7312627a7ba61590a078d61df090706
= <dhakatedeep909@gmail.com>
2023-11-18 12:01:25 +0530

    Update

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "../App.css";
 import heroImg from "../images/profile.png";
-import expcardimg1 from "./../images/vtf-image.png";
-import expcardimg2 from "./../images/owasp-img.png";
-import expcardimg3 from "./../images/h1.jpg";
+import ExpData from "../Data/Experience";
 import GearImg from "./../images/gears.png";
 import Mail from "./../images/mail.png";
 import Linkedin from "./../images/linkedin.png";
@@ -429,45 +427,6 @@ function ExitExpDetails(e) {
   document.querySelector(newIdImg).style.height = "0%";
   document.querySelector(newIdDetails).style.height = "0%";
 }
-
-const ExpData = [
-  {
-    id: 1,
-    title: "Virtual Testing Foundation",
-    role: "CyberSecurity Intern",
-    loc: "remote",
-    date: "JAN 18 2021 - MAR 18 2021",
-    img: expcardimg1,
-    classDis: "expcard1 expcard",
-    color: "red",
-    details:
-      "VTF is a California, USA based 501(c)3 nonprofit volunteer-run organization. Over the internship period I have completed,<br><br>● Kube Academy Courses<br><br>● Hands-on lab learning<br/><br/>● Digital and social media marketing<br/><br/>● Captured minutes of meeting and had team collaborations",
-  },
-  {
-    id: 2,
-    title: "OWASP Chapter PCCOE",
-    role: "Security Team Lead",
-    loc: "PCCOE",
-    date: "JULY 25 2021 - CURRENT",
-    img: expcardimg2,
-    classDis: "expcard2 expcard",
-    color: "blue",
-    details:
-      "The Open Web Application Security Project® (OWASP) is a nonprofit foundation that works to improve the security of software. Being a part of Security & CTF Team, my responsibilities are,<br/><br/>● Organize Tech-talks<br/><br/>● Lead University CTF Team<br/><br/>● Organize Security Workshops & Bootcamps<br/><br/>● Create Awareness Regarding Security Domain",
-  },
-  {
-    id: 3,
-    title: "Hacktivitycon Rank 45",
-    role: "CTF Team Lead",
-    loc: "REMOTE",
-    date: "SEPT 17 2021 - SEPT 19 2021",
-    img: expcardimg3,
-    classDis: "expcard1 expcard",
-    color: "purple",
-    details:
-      "H@ctivitycon is a HackerOne hosted hacker conference built by the community for the community. We scored 5527 points and stood 45th all over world. We PWNed the following categories,<br/><br/>● Forensics, OSINT<br/><br/>● Web Vulnerabilities<br/><br/>● Cryptography, Reverse Engineering<br/><br/>● Mobile App Vulnerabilities.",
-  },
-];
 
 function Skills() {
   const listSkill = SkillData.map((item) => {
